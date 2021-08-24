a0bb949985f406e447f85b5ace11d5e853715d23
Subhojit Paul <subhojitpaul21@gmail.com>
2021-08-24 10:09:25 +0530

    userdao custom queries

--- src/main/java/com/upgrad/hirewheels/dao/UserDao.java
+++ src/main/java/com/upgrad/hirewheels/dao/UserDao.java
@@ -3,4 +3,14 @@ package com.upgrad.hirewheels.dao;
 import com.upgrad.hirewheels.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDao extends JpaRepository<User, Integer> { }
+import java.util.List;
+
+public interface UserDao extends JpaRepository<User, Integer> {
+    public List<User> findAllByFirstName(String firstName);
+
+    public List<User> findAllByFirstNameOrLastName(String firstName, String lastName);
+
+    public User findByEmail(String email);
+
+    public User findByMobileNo(String mobileNo);
+}
